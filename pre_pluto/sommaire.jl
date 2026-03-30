e9c7294bb526731201b50da36b0d669fbb2bfcd3
Jeanne Laflamme <jmlaflam@uwaterloo.ca>
2026-03-30 18:24:16 -0400

    new page division

---
+++ pre_pluto/sommaire.jl
@@ -0,0 +1,50 @@
+### A Pluto.jl notebook ###
+# v0.20.21
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 7ad5466a-2c80-11f1-b3a3-0f33b10e3fab
+md"""
+	!!! info "Sommaire de la proposition"
+		##### AM
+		---
+		###### Parcours 14 *(ULaval via Shannon)*
+		- **`Deux`** départs à **`6h15`** et **`8h`**
+	
+		###### Parcours 14X *(Route Ste-Geneviève)*
+		- **`Un`** départ à **`8h20`** et **`10h45`**
+	
+		###### Parcours 13 *(Saint-Augustin)*
+		- Conserve **`deux`** départs (sur trois) à **`6h`** et **`7h`**
+	
+			 
+		##### PM
+		---
+	
+		###### Parcours 14 *(ULaval via Shannon)*
+	    - **`Deux`** retours à **`16h40`** et **`18h40`**
+	
+		###### Parcours 14X *(Route Ste-Geneviève)*
+		- **`Trois`** retours à **`12h45`**, **`20h05`** et **`21h05`**
+	
+		###### Parcours 13 *(Saint-Augustin)*
+	    - Conserve **`deux`** retours (sur cinq) à **`15h30`** et **`16h32`**
+	
+		---
+		**KILOMÉTRAGE TOTAL DE LA PROPOSITION** `580 km`
+			 
+		**HEURES TRAVAILLÉES PAR LES CHAUFFEURS** `7 x 3h = 21h`
+			 
+		----
+		###### Actuellement (Parcours 12 et 33)
+		 - kilométrage `380 km`
+		 - blocs chauffeurs `7 x 3h`
+		---
+		###### Au 1er avril (Parcours 13 et 33)
+		 - kilométrage `372 km`
+		 - blocs chauffeurs `7 x 3h`
+	"""
+
+# ╔═╡ Cell order:
+# ╟─7ad5466a-2c80-11f1-b3a3-0f33b10e3fab
